c76a9411a659f3cd0ab85fcbe2000f22ffa98049
Prabesh355 <prabeshmsb76@gmail.com>
2026-05-29 17:38:51 +0545

    Feature: include Three Pass Trek in home featured destinations

--- himalayan-vista-ui/src/routes/index.tsx
+++ himalayan-vista-ui/src/routes/index.tsx
@@ -153,7 +153,7 @@ function FeaturedDestinations() {
       </div>
 
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {destinations.slice(0, 6).map((d, i) => (
+        {destinations.slice(0, 8).map((d, i) => (
           <DestinationCard key={d.id} d={d} index={i} />
         ))}
       </div>
