864fbca93f4962c5340a7ebe5a0f9341c72be37b
Prabesh355 <prabeshmsb76@gmail.com>
2026-06-02 20:32:33 +0545

    ui: update app logo to clearer Nomads Navigate Nepal stamp

--- himalayan-vista-ui/src/components/layout/Footer.tsx
+++ himalayan-vista-ui/src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 import { Link } from "@tanstack/react-router";
 import { Facebook, Instagram, Twitter, Youtube, Mail } from "lucide-react";
-import logo from "@/assets/logo.svg";
+import logo from "@/assets/NOMADS NAVIGATE  NEPAL STAMP.webp";
 
 export function Footer() {
   return (
